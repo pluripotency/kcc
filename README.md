ed69bc9e4410a032068f90feac2d2dbaec115ec7
Masayoshi Kokuryo <masakoku@gmail.com>
2020-02-16 09:55:28 +0900

    ステップ6からステップ毎のプロジェクトに分割

---
+++ README.md
@@ -0,0 +1,14 @@
+# C Compiler作成入門
+
+C言語に詳しくなくてもC言語のコンパイラを作成できる入門記事です
+
+https://www.sigbus.info/compilerbook#docker
+
+ステップ6からステップごとのプロジェクトに分割しました
+
+
+## Dockerで動かす
+スクリプトの引数でステップフォルダを指定してください。
+```
+sh run_docker step6
+```
--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-#docker build -t compilerbook https://www.sigbus.info/compilerbook/Dockerfile
-docker run --rm -v $HOME/Projects/kcc:/kcc -w /kcc compilerbook make test
+if [ `docker images -q compilerbook | wc -l` -eq 1 ]; then 
+  echo Prepararing compilerbook docker image
+  docker build -t compilerbook https://www.sigbus.info/compilerbook/Dockerfile
+fi
+
+if [ -d `pwd`/$1 ]; then
+  WORK_DIR=`pwd`/$1
+else
+  if [ -d `pwd`/step$1 ]; then
+    WORK_DIR=`pwd`/step$1
+  else
+    echo "usage: $0 [step number]"
+    exit 1
+  fi
+fi
+
+echo Starting run_docker on $WORK_DIR
+docker run --rm -v ${WORK_DIR}:/kcc -w /kcc compilerbook make test
